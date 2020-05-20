ef8ebe46e272c93b4194de0e93881fdcbb68b8b9
nutcracker89 <65375221+nutcracker89@users.noreply.github.com>
2020-05-20 14:26:28 +0300

    Update esx_shops.sql

--- esx_policeshop/esx_shops.sql
+++ esx_policeshop/esx_shops.sql
@@ -1,5 +1,14 @@
 USE `essentialmode`;
 
+CREATE TABLE IF NOT EXISTS `shops` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`store` varchar(100) NOT NULL,
+	`item` varchar(100) NOT NULL,
+	`price` int NOT NULL,
+
+	PRIMARY KEY (`id`)
+);
+
 
 INSERT INTO `shops` (store, item, price) VALUES
 	('Policeshop','bread',30),
